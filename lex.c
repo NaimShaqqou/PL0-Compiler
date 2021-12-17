fd89c4c058eb0f25bd61bcec2d2d4a5ea5295dde
Naim Shaqqou <70739148+NaimShaqqou@users.noreply.github.com>
2021-12-17 15:18:25 -0500

    Update lex.c

--- lex.c
+++ lex.c
@@ -1,14 +1,3 @@
-/*
-	This is the lex.c file for the UCF Fall 2021 Systems Software Project.
-	For HW2, you must implement the function lexeme *lexanalyzer(char *input).
-	You may add as many constants, global variables, and support functions
-	as you desire.
-
-	If you choose to alter the printing functions or delete list or lex_index,
-	you MUST make a note of that in you readme file, otherwise you will lose
-	5 points.
-*/
-
 /*****************
  * Group Members:
  * Naim Shaqqou
